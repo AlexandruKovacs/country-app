418497de6086d3e5835b5c9bc073e1aa49566738
Alex Kovacs <axkovacs27@gmail.com>
2023-07-21 22:48:35 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingMOdule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { CountriesModule } from './countries/countries.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +14,8 @@ import { CountriesModule } from './countries/countries.module';
   imports: [
     BrowserModule,
     AppRoutingMOdule,
+    HttpClientModule,
+
     SharedModule,
   ],
   providers: [],
